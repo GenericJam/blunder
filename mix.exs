55221071b7e03095bbfcc66a614f406efa932561
Marcos Tapajos <mtapajos@decisiv.com>
2018-09-13 08:04:45 -0300

    Make it a minor bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blunder.Mixfile do
     [
       app: :blunder,
       name: "Blunder",
-      version: "1.0.4",
+      version: "1.1.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
